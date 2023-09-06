5e4f13f49bdf2c7ff0a4be39daecdecbc3589d98
vanna567 <112666892+vanna567@users.noreply.github.com>
2023-09-05 18:15:08 -0600

    Add files via upload

---
+++ tira un dado.py
@@ -0,0 +1,46 @@
+import random
+
+response = "y"
+while response == "y":
+
+  no = random.randit(1,6)
+  
+  if no == 1:
+    print("[-----]")
+    print("[     ]")
+    print("[  0  ]")
+    print("[     ]")
+    print("[-----]")
+  if no == 1:
+    print("[-----]")
+    print("[0    ]")
+    print("[     ]")
+    print("[    0]")
+    print("[-----]")
+  if no == 1:
+    print("[-----]")
+    print("[0    ]")
+    print("[  0  ]")
+    print("[    0]")
+    print("[-----]")
+  if no == 1:
+    print("[-----]")
+    print("[0   0]")
+    print("[     ]")
+    print("[0   0]")
+    print("[-----]")
+  if no == 1:
+    print("[-----]")
+    print("[0   0]")
+    print("[  0  ]")
+    print("[0   0]")
+    print("[-----]")
+  if no == 1:
+    print("[-----]")
+    print("[0   0]")
+    print("[0   0]")
+    print("[0   0]")
+    print("[-----]")
+
+response = input("presiona ¨y¨ para poder tirar de nuevo, y n para salir:")
+print ("\n")
